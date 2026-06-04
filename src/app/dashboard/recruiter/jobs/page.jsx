7af8af113b487a4424ef8dd0f33008c392d0af62
foysal41 <mdfoysal.mf33@gmail.com>
2026-06-04 08:54:12 +0600

    added job post page wih data

---
+++ src/app/dashboard/recruiter/jobs/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RecruiterJobs() {
+  return (
+    <div>
+        <h2>Recruiter/Company manage all Jobs</h2>
+    </div>
+  )
+}
+
+export default RecruiterJobs
