5a4bbc7273ad7ec8c6f9291c347caf1317eb8179
foysal41 <mdfoysal.mf33@gmail.com>
2026-06-15 21:43:20 +0600

    61.3: implement JWT auth flow for secure API communication - Extract user token from active session - Generate authorization headers dynamically - Include JWT token in server mutations for protected routes

--- src/lib/core/server.js
+++ src/lib/core/server.js
@@ -1,7 +1,22 @@
+import { getUserToken } from "./session";
+
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const authHeader = async()=>{
+    const token = await getUserToken()
+    const header = {
+        authorization: `Bearer${token}`
+    }
+    return token?header :{};
+}
+
+
 export const serverFetch = async (path)=> {
     const res = await fetch(`${baseUrl}${path}`);
-    return res.json();
+     // Handle empty response safely
+  const text = await res.text();
+
+  return text ? JSON.parse(text) : null;
 }
 
 export const serverMutation = async (path,data) => {
@@ -9,6 +24,7 @@ export const serverMutation = async (path,data) => {
         method:'POST',
         headers:{
             'Content-Type': 'application/json',
+            ...await authHeader()
         },
         body:JSON.stringify(data)
 
--- src/lib/core/session.js
+++ src/lib/core/session.js
@@ -2,13 +2,25 @@ import { headers } from "next/headers";
 import { auth } from "../auth";
 import { redirect } from "next/navigation";
 
+
+
 export const getUserSession = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-
   return session?.user || null;
 };
+
+
+export const getUserToken = async() =>{
+  const session =await auth.api.getSession({
+    header: await headers()
+  })
+
+
+  return session?.session?.token || null;
+}
+
 
 
 export const requireRole = async (role) => {
